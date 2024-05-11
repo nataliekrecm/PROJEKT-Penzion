cdce30519b5604882de85858434a62dd1ca4f315
nataliekrecmerova <krecmerova.natalie@gmail.com>
2024-05-11 18:21:08 +0200

    eleventy

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function(eleventyConfig) {
+ 
+ eleventyConfig.addPassthroughCopy("images");
+ eleventyConfig.addPassthroughCopy("css");
+
+  return {
+    
+    templateFormats: ["njk", "html", "md"],
+
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  }
+};
+
+
